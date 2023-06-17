6f4b8551e6ba38f22d45a731c803618eb034ecb6
saqqdy <saqqdy@qq.com>
2023-06-17 14:28:25 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,96 +2,107 @@ import { ref, shallowRef, watch } from 'vue-demi'
 import type { Ref, ShallowRef } from 'vue-demi'
 import { inBrowser, isChrome } from './utils'
 
-export interface SpeechOptions {
-	preferTouchEvent: boolean
-	voiceFilter: ((voice: SpeechSynthesisVoice) => boolean) | null
-	lang: 'zh-CN' | string
-	pitch: number
-	rate: number
-	volume: number
+declare global {
+	interface Window {
+		graceRecognitionReady: boolean
+		SpeechRecognition: SpeechRecognition
+		webkitSpeechRecognition: SpeechRecognition
+	}
 }
-export type UtterOptions = Partial<
-	Pick<
-		SpeechSynthesisUtterance,
-		| 'lang'
-		| 'onboundary'
-		| 'onend'
-		| 'onerror'
-		| 'onmark'
-		| 'onpause'
-		| 'onresume'
-		| 'onstart'
-		| 'pitch'
-		| 'rate'
-		| 'text'
-		| 'voice'
-		| 'volume'
-	>
->
-export interface Effect {
-	key: symbol
-	content: string
-	utterOptions: UtterOptions
+
+export type RecognitionEventType =
+	| 'start'
+	| 'audiostart'
+	| 'soundstart'
+	| 'speechstart'
+	| 'result'
+	| 'speechend'
+	| 'soundend'
+	| 'audioend'
+	| 'end'
+	| 'error'
+	| 'nomatch'
+
+export interface RecognitionOptions {
+	preferTouchEvent: boolean
+	lang: 'zh-CN' | string
+	interimResults: boolean
+	maxAlternatives: number
+	continuous: boolean
+	onStatusChange?: (
+		status: RecognitionEventType,
+		event: Event | SpeechRecognitionEvent | SpeechRecognitionErrorEvent
+	) => void
 }
 
 let ready: Ref<boolean>,
-	voice: ShallowRef<SpeechSynthesisVoice | undefined>,
-	utter: Ref<SpeechSynthesisUtterance | null>,
-	speech: SpeechSynthesis,
-	effects: Ref<Effect[]>
-const defaultOptions: SpeechOptions = {
+	status: Ref<RecognitionEventType>,
+	result: Ref<string>,
+	recognition: SpeechRecognition
+const defaultOptions: RecognitionOptions = {
 	preferTouchEvent: false,
-	voiceFilter: null,
 	lang: 'zh-CN',
-	pitch: 1,
-	rate: 1,
-	volume: 1
+	interimResults: false,
+	maxAlternatives: 1,
+	continuous: false
 }
 
-function useRecognition(options: SpeechOptions) {
+function useRecognition(options: RecognitionOptions) {
 	if (!inBrowser) return
-	if (typeof window.speechSynthesis === 'undefined') {
-		console.error('speechSynthesis is not supported')
+	if (typeof window.SpeechRecognition !== 'undefined') {
+		recognition = new SpeechRecognition()
+	} else if (typeof window.webkitSpeechRecognition !== 'undefined') {
+		// eslint-disable-next-line new-cap
+		recognition = new webkitSpeechRecognition()
+	} else {
+		console.error('SpeechRecognition is not supported')
 		return
 	}
 
 	options = Object.assign(defaultOptions, options || {})
-	if (!speech) speech = window.speechSynthesis
-	if (!effects) effects = ref([])
-	if (!utter) utter = ref(null)
-	if (!voice) {
-		voice = shallowRef()
-		getVoice().then(voices => (voice.value = voices))
-	}
+	recognition.lang = options.lang
+	recognition.interimResults = options.interimResults
+	recognition.maxAlternatives = options.maxAlternatives
+	recognition.continuous = options.continuous
+
+	bindHandler()
+
 	if (!ready) {
 		ready = ref(!isChrome)
 
 		// exec
-		watch([ready, voice, effects], ([isReady, voiceValue, effectsValue]) => {
-			if (isReady && voiceValue && effectsValue.length > 0) {
-				for (const { content, utterOptions } of effectsValue) {
-					utter.value = new SpeechSynthesisUtterance(content)
-					let action: keyof UtterOptions
-					for (action in utterOptions) {
-						utter.value[action] = utterOptions[action] as never
-					}
-					utter.value.voice = voiceValue
-					utter.value.pitch = options.pitch
-					utter.value.rate = options.rate
-					utter.value.volume = options.volume
-					speech.speak(utter.value)
-				}
-				effects.value = []
-				utter.value = null
-			}
-		})
+		// watch([ready, voice, effects], ([isReady, voiceValue, effectsValue]) => {
+		// 	if (isReady && voiceValue && effectsValue.length > 0) {
+		// 		for (const { content, utterOptions } of effectsValue) {
+		// 			utter.value = new SpeechSynthesisUtterance(content)
+		// 			let action: keyof UtterOptions
+		// 			for (action in utterOptions) {
+		// 				utter.value[action] = utterOptions[action] as never
+		// 			}
+		// 			utter.value.voice = voiceValue
+		// 			utter.value.pitch = options.pitch
+		// 			utter.value.rate = options.rate
+		// 			utter.value.volume = options.volume
+		// 			speech.speak(utter.value)
+		// 		}
+		// 		effects.value = []
+		// 		utter.value = null
+		// 	}
+		// })
 	}
 	if (!ready.value) {
 		const eventName = options.preferTouchEvent ? 'touchend' : 'click'
 		// init
-		const handler = () => {
-			speech.speak(new SpeechSynthesisUtterance(''))
-			ready.value = speech.speaking || speech.pending
+		const handler = (
+			event:
+				| MouseEvent
+				| TouchEvent
+				| CompositionEvent
+				| FocusEvent
+				| InputEvent
+				| KeyboardEvent
+		) => {
+			ready.value = window.graceRecognitionReady = event.isTrusted
 			if (ready.value) {
 				window.removeEventListener(eventName, handler)
 				window.removeEventListener('keypress', handler)
@@ -102,113 +113,86 @@ function useRecognition(options: SpeechOptions) {
 	}
 
 	/**
-	 * get voice
-	 *
-	 * @returns result voice: SpeechSynthesisVoice
+	 * bind handler
 	 */
-	async function getVoice(): Promise<SpeechSynthesisVoice | undefined> {
-		if (voice.value) return voice.value
-
-		let voices = speech.getVoices()
-		if (!voices || !voices.length) {
-			await new Promise(resolve =>
-				speech.addEventListener('voiceschanged', resolve, { once: true })
-			)
-			voices = speech.getVoices()
+	function bindHandler() {
+		recognition.onresult = (event: SpeechRecognitionEvent) => {
+			status.value = event.type as RecognitionEventType
+			result.value = ([] as any)
+				.concat(event.results)
+				.map((item: SpeechRecognitionResult) => item[0].transcript)
+				.join('|') // event.results[0][0].transcript;
+			options.onStatusChange && options.onStatusChange('result', event)
 		}
-
-		voices = voices.sort((a, b) => {
-			const nameA = a.name.toUpperCase()
-			const nameB = b.name.toUpperCase()
-			if (nameA < nameB) return -1
-			else if (nameA === nameB) return 0
-			return 1
-		})
-
-		voice.value = options.voiceFilter
-			? voices.find(options.voiceFilter)
-			: voices.find(({ lang, localService }) => localService && lang === options.lang) ||
-			  voices.find(({ lang }) => lang === options.lang)
-		return voice.value
-	}
-
-	/**
-	 * get current utter
-	 *
-	 * @returns result - utter: SpeechSynthesisUtterance
-	 */
-	function getCurrentUtter(): SpeechSynthesisUtterance | null {
-		if (!utter.value) console.warn('no utter right now')
-		return utter.value
-	}
-
-	/**
-	 * speak
-	 *
-	 * @param content - speak text
-	 * @param utterOptions - utter options: UtterOptions
-	 * @returns result - effectKey: symbol
-	 */
-	function speak(content: string, utterOptions: UtterOptions = {}): Effect['key'] {
-		const effect = {
-			key: Symbol('SpeechKey#effect'),
-			content,
-			utterOptions
+		recognition.onspeechend = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('speechend', event)
 		}
-		effects.value = ([] as Effect[]).concat(effects.value, effect)
-
-		return effect.key
-	}
-
-	/**
-	 * Remove unconsumed speak
-	 *
-	 * @param effectKey - key of effect
-	 * @returns result - cancellation result true=Cancellation success false=Broadcast content not found or broadcast consumed
-	 */
-	function remove(effectKey: Effect['key']): boolean {
-		const _effects = ([] as Effect[]).concat(effects.value)
-		const index = _effects.findIndex(({ key }) => key === effectKey)
-		if (index > -1) {
-			_effects.splice(index, 1)
-			effects.value = _effects
-			return true
+		recognition.onnomatch = (event: SpeechRecognitionEvent) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('nomatch', event)
 		}
-		return false
+		recognition.onerror = (event: SpeechRecognitionErrorEvent) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('error', event)
+		}
+		recognition.onaudioend = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('audioend', event)
+		}
+		recognition.onaudiostart = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('audiostart', event)
+		}
+		recognition.onend = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('end', event)
+		}
+		recognition.onsoundend = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('soundend', event)
+		}
+		recognition.onsoundstart = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('soundstart', event)
+		}
+		recognition.onspeechstart = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('speechstart', event)
+		}
+		recognition.onstart = (event: Event) => {
+			status.value = event.type as RecognitionEventType
+			options.onStatusChange && options.onStatusChange('start', event)
+		}
 	}
 
 	/**
-	 * Forced cancellation of all broadcasts, and immediate cancellation of those being broadcast
+	 * Starts the speech recognition service listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
 	 */
-	function cancel() {
-		speech.cancel()
+	function start() {
+		recognition.start()
 	}
 
 	/**
-	 * Suspension of all broadcasts and immediate cancellation of those being broadcast
+	 * Stops the speech recognition service from listening to incoming audio, and doesn't attempt to return a SpeechRecognitionResult.
 	 */
-	function pause() {
-		speech.pause()
+	function abort() {
+		recognition.abort()
 	}
 
 	/**
-	 * Resume all broadcasts and immediately cancel those being broadcast
+	 * Stops the speech recognition service from listening to incoming audio, and attempts to return a SpeechRecognitionResult using the audio captured so far.
 	 */
-	function resume() {
-		speech.resume()
+	function stop() {
+		recognition.stop()
 	}
 
 	return {
 		ready,
-		voice,
-		speech,
-		speak,
-		remove,
-		cancel,
-		pause,
-		resume,
-		getVoice,
-		getCurrentUtter
+		recognition,
+		start,
+		abort,
+		stop
 	}
 }
 
